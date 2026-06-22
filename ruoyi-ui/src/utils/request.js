837a4dff7571f00d8ef58ab7032627fc796e02f1
xjtu2024zlzs <3476653740@qq.com>
2026-06-22 16:01:14 +0800

    修改登陆逻辑

--- ruoyi-ui/src/utils/request.js
+++ ruoyi-ui/src/utils/request.js
@@ -22,6 +22,9 @@ const service = axios.create({
 
 // request拦截器
 service.interceptors.request.use(config => {
+  if (config.data instanceof FormData) {
+    delete config.headers['Content-Type'];
+  }
   // 是否需要设置 token
   const isToken = (config.headers || {}).isToken === false
   // 是否需要防止数据重复提交
